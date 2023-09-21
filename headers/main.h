30dec8234047c9ed5efd8be7f01142a031fcdef9
alex <Ocearnus@gmail.com>
2023-09-21 20:27:00 +0100

    Moved build system from make to cmake.

---
+++ headers/main.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "ppmFileGen.h"
+#include "timer.h"
+#include "raytracer.h"
+
+#include "../../libs/eigen/Eigen/Dense"
+
+#include <iostream>
+#include <cstdint>
+#include <array>
+
+using Eigen::Vector2d;
+using Eigen::Vector3d;
+using Eigen::Vector4d;
